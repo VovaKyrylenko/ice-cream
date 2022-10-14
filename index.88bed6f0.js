2bd42f9d753aea445d9a5b18238927084297b0c6
VovaKyrylenko <110337169+VovaKyrylenko@users.noreply.github.com>
2022-10-14 20:35:18 +0000

    Deploying to gh-pages from @ VovaKyrylenko/ice-cream@b4ae67d650d6ef218d723516c40566ec148c2e37 🚀

---
+++ index.88bed6f0.js
@@ -0,0 +1,2 @@
+$(".customer").slick({dots:!0,prevArrow:!1,nextArrow:!1,infinite:!0,speed:500,cssEase:"linear"});
+//# sourceMappingURL=index.88bed6f0.js.map
---
+++ index.fba9fccf.js
@@ -0,0 +1,2 @@
+$(".customer").slick({dots:!0,prevArrow:!1,nextArrow:!1,infinite:!0,speed:500,cssEase:"linear"});
+//# sourceMappingURL=index.fba9fccf.js.map
