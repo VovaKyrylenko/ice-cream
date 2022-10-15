720c19df70ee963c4a48846ed9edf4e29678084f
unknown <myron.melnyk.kn.2021@lpnu.ua>
2022-10-15 12:34:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 $('.customer').slick({
-    dots: true,
-    prevArrow: false,
-    nextArrow: false,
-    infinite: true,
-    speed: 500,
-    cssEase: 'linear'
-  });
+   dots: true,
+   prevArrow: false,
+   nextArrow: false,
+   infinite: true,
+   speed: 850,
+   cssEase: 'ease-in',
+   autoplay: true,
+   autoplaySpeed: 3000,
+});
+
+// ------------- Змінює класи Slider відповідно до БЕМ -------------
+(() => {
+   const refs = {
+      customer__dots: document.querySelector('.slick-dots'),
+      customer__dot: document.querySelectorAll('.slick-dots li'),
+      customer__button: document.querySelectorAll('.slick-dots button'),
+   };
+
+   length = refs.customer__dots.children.length;
+   refs.customer__dots.classList.add('customer__dots');
+   refs.customer__dots.removeAttribute('style');
+
+   for (let i = 0; i < length; i++) {
+      refs.customer__dot[i].classList.add('customer__dot');
+      refs.customer__button[i].style.display = 'none';
+   }
+
+   window.setInterval(() => {
+      for (let i = 0; i < length; i++) {
+         if (refs.customer__dot[i].classList.contains('slick-active'))
+            refs.customer__dot[i].classList.add('customer__dot--active');
+         else refs.customer__dot[i].classList.remove('customer__dot--active');
+      }
+   }, 100);
+})();
