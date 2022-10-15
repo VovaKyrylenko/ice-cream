5e80abcb061cea6ca92c58625c144160c0ee0012
unknown <myron.melnyk.kn.2021@lpnu.ua>
2022-10-15 19:14:18 +0300

    com-2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+$('.gallery-js').slick({ 
+   dots: false, 
+   infinite: true, 
+   arrows: false, 
+   speed: 300, 
+   fade: true, 
+   slidesToShow: 1, 
+   adaptiveHeight: true, 
+   autoplay: true, 
+   autoplaySpeed: 3000, 
+   cssEase: 'ease-in',
+ });
+
 $('.customer').slick({
    dots: true,
    prevArrow: false,
